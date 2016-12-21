57525326686234c887e91a0049ea41792083cf05
Iñaki Úcar <i.ucar86@gmail.com>
2016-12-21 15:25:10 +0100

    fix version comparing

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,7 @@ library(testthat)
 library(simmer)
 library(simmer.plot)
 
-if (-1 == utils::compareVersion("3.5.1", as.character(utils::packageVersion("simmer"))))
+if (-1 < utils::compareVersion("3.5.1", as.character(utils::packageVersion("simmer"))))
   trajectory <- create_trajectory
 
 test_check("simmer.plot")
