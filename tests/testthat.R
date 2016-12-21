1300fc9528bc6b643c5375ea51d33ea557be4d30
Iñaki Úcar <i.ucar86@gmail.com>
2016-12-21 15:40:09 +0100

    no need to detach

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,6 +6,3 @@ if (-1 < utils::compareVersion("3.5.1", as.character(utils::packageVersion("simm
   trajectory <- create_trajectory
 
 test_check("simmer.plot")
-
-detach("package:simmer.plot", unload = TRUE)
-detach("package:simmer", unload = TRUE)
