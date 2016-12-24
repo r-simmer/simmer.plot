f9d89a32f3478932d8c07ad340bac188f6a1fc79
Iñaki Úcar <i.ucar86@gmail.com>
2016-12-24 02:19:45 +0100

    try with visNetwork

--- working_dir/__working_file.R
+++ working_dir/__working_file.R
@@ -4,7 +4,7 @@ t0 <- trajectory() %>%
   seize("nurse", 1) %>%
   timeout(function() rnorm(1, 15)) %>%
   release("nurse", 1) %>%
-  branch(function() 1, TRUE,
+  branch(function() 1, c(T, T),
          trajectory() %>%
            seize("doctor", function() 1) %>%
            timeout(function() rnorm(1, 20)) %>%
@@ -13,44 +13,51 @@ t0 <- trajectory() %>%
                   trajectory() %>%
                     seize("administration", 1) %>%
                     timeout(1) %>%
-                    release("administration", 1))) %>%
-  rollback(1) %>%
-  rollback(1, check = function() FALSE) %>%
-  set_attribute("dummy", 1) %>%
-  set_attribute("dummy", function() 1)
+                    release("administration", 1)),
+         trajectory() %>%
+           rollback(1) %>%
+           rollback(1, check = function() FALSE) %>%
+           set_attribute("dummy", 1) %>%
+           set_attribute("dummy", function() 1)) %>%
+  timeout(1)
 
 ########################################################
 
-library(ggnetwork)
-library(dplyr)
+library(visNetwork)
 
 t0$verbose <- TRUE
 out <- utils::capture.output(t0)
 out <- out[grep("0x", out)]
-out <- sub(".*Activity: ", "", out)
-activities <- as.data.frame(sub(" .*", "", out))
-colnames(activities) <- "name"
-out <- sub(".*<- ", "", out)
-pointers <- sub(" \\|.*", "", out)
-pointers <- t(as.data.frame(strsplit(pointers, " -> ")))
-rownames(pointers) <- NULL
-colnames(pointers) <- c("id", "to")
-dt <- cbind(activities, pointers)
-dt$id <- as.character(dt$id)
-dt$to <- as.character(dt$to)
-dt$to[[grep("0 .*", dt$to)]] <- NA
-dt$xend <- dt$x <- runif(nrow(dt))
-dt$yend <- dt$y <- nrow(dt):1
-connections <- do.call(rbind, lapply(which(!is.na(dt$to)), function(i) {
-  row <- dt[i,]
-  to <- dt[dt$id==row$to,]
-  row$xend <- if_else(row$xend > to$x, to$x + 0.04, to$x - 0.04)
-  row$yend <- to$y + 0.4
-  row
-}))
 
-ggplot(rbind(dt, connections), aes(x=x, y=y, xend=xend, yend=yend, label=name)) +
-  geom_edges(arrow = arrow(length = unit(6, "pt"), type = "closed")) +
-  #geom_nodes() +
-  geom_nodelabel() +
-  theme_blank()
+out <- sub(".*Activity: ", "", out)
+nodes <- as.data.frame(sub(" .*", "", out), stringsAsFactors=FALSE)
+colnames(nodes) <- "label"
+nodes$shape <- "box"
+
+out <- sub("[[:alpha:]]*[[:space:]]*\\| ", "", out)
+b_edges <- sub(" ->.*", "", out)
+b_edges <- t(as.data.frame(strsplit(b_edges, " <- ")))
+b_edges <- as.data.frame(b_edges, stringsAsFactors=FALSE)
+b_edges <- tail(b_edges, -1)
+rownames(b_edges) <- NULL
+colnames(b_edges) <- c("from", "to")
+
+out <- sub(".*<- ", "", out)
+f_edges <- sub(" \\|.*", "", out)
+f_edges <- t(as.data.frame(strsplit(f_edges, " -> ")))
+f_edges <- as.data.frame(f_edges, stringsAsFactors=FALSE)
+rownames(f_edges) <- NULL
+colnames(f_edges) <- c("from", "to")
+nodes$id <- f_edges$from
+f_edges <- head(f_edges, -1)
+
+forks <- as.data.frame(f_edges != b_edges)
+add <- which(forks$to)
+dashes <- which(forks$to & !forks$from)
+
+edges <- rbind(f_edges, b_edges[add,])
+edges$arrows <- "to"
+edges$dashes <- FALSE
+edges[dashes,]$dashes <- TRUE
+
+visNetwork(nodes, edges)
