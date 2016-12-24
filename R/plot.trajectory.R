e8ca042a22928e13d3d0ef290472c916b98fed66
Iñaki Úcar <i.ucar86@gmail.com>
2016-12-24 19:28:21 +0100

    first approach with DiagrammeR

--- R/plot.trajectory.R
+++ R/plot.trajectory.R
@@ -1,3 +1,39 @@
+#' @export
 plot.trajectory <- function(x, ...) {
-  print("Nothing yet")
+  old_verbose <- x$verbose
+  x$verbose <- TRUE
+  out <- utils::capture.output(x)
+  x$verbose <- old_verbose
+  out <- out[grep("0x", out)]
+
+  #level <- nchar(sub("\\{.*", "", out)) / 2
+  out <- sub(".*Activity: ", "", out)
+  nodes <- as.data.frame(sub(" .*", "", out), stringsAsFactors=FALSE)
+  colnames(nodes) <- "label"
+  nodes$shape <- "box"
+
+  out <- sub("[[:alpha:]]*[[:space:]]*\\| ", "", out)
+  b_edges <- sub(" ->.*", "", out)
+  b_edges <- t(as.data.frame(strsplit(b_edges, " <- ")))
+  b_edges <- as.data.frame(b_edges, stringsAsFactors=FALSE)
+  colnames(b_edges) <- c("from", "to")
+  nodes$nodes <- b_edges$to
+  b_edges <- utils::tail(b_edges, -1)
+  rownames(b_edges) <- NULL
+
+  out <- sub(".*<- ", "", out)
+  f_edges <- sub(" \\|.*", "", out)
+  f_edges <- t(as.data.frame(strsplit(f_edges, " -> ")))
+  f_edges <- as.data.frame(f_edges, stringsAsFactors=FALSE)
+  rownames(f_edges) <- NULL
+  colnames(f_edges) <- c("from", "to")
+  f_edges <- f_edges[-grep("0 +", f_edges$to),]
+
+  edges <- unique(rbind(f_edges, b_edges))
+  edges$color <- "black"
+  #edges$dashes <- FALSE
+  #edges[dashes,]$dashes <- TRUE
+
+  graph <- DiagrammeR::create_graph(nodes, edges)
+  DiagrammeR::render_graph(graph, ...)
 }
--- working_dir/__working_file.R
+++ working_dir/__working_file.R
@@ -4,7 +4,7 @@ t0 <- trajectory() %>%
   seize("nurse", 1) %>%
   timeout(function() rnorm(1, 15)) %>%
   release("nurse", 1) %>%
-  branch(function() 1, c(T, T),
+  branch(function() 1, c(T, F),
          trajectory() %>%
            seize("doctor", function() 1) %>%
            timeout(function() rnorm(1, 20)) %>%
@@ -23,41 +23,4 @@ t0 <- trajectory() %>%
 
 ########################################################
 
-library(visNetwork)
-
-t0$verbose <- TRUE
-out <- utils::capture.output(t0)
-out <- out[grep("0x", out)]
-
-out <- sub(".*Activity: ", "", out)
-nodes <- as.data.frame(sub(" .*", "", out), stringsAsFactors=FALSE)
-colnames(nodes) <- "label"
-nodes$shape <- "box"
-
-out <- sub("[[:alpha:]]*[[:space:]]*\\| ", "", out)
-b_edges <- sub(" ->.*", "", out)
-b_edges <- t(as.data.frame(strsplit(b_edges, " <- ")))
-b_edges <- as.data.frame(b_edges, stringsAsFactors=FALSE)
-b_edges <- tail(b_edges, -1)
-rownames(b_edges) <- NULL
-colnames(b_edges) <- c("from", "to")
-
-out <- sub(".*<- ", "", out)
-f_edges <- sub(" \\|.*", "", out)
-f_edges <- t(as.data.frame(strsplit(f_edges, " -> ")))
-f_edges <- as.data.frame(f_edges, stringsAsFactors=FALSE)
-rownames(f_edges) <- NULL
-colnames(f_edges) <- c("from", "to")
-nodes$id <- f_edges$from
-f_edges <- head(f_edges, -1)
-
-forks <- as.data.frame(f_edges != b_edges)
-add <- which(forks$to)
-dashes <- which(forks$to & !forks$from)
-
-edges <- rbind(f_edges, b_edges[add,])
-edges$arrows <- "to"
-edges$dashes <- FALSE
-edges[dashes,]$dashes <- TRUE
-
-visNetwork(nodes, edges)
+plot(t0)
