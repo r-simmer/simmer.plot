a9e7fa08dccb9adf68bf9d0b847ceda329420060
Iñaki Úcar <i.ucar86@gmail.com>
2016-12-30 18:52:15 +0100

    improve testing

--- tests/testthat/test-plot-trajectory.R
+++ tests/testthat/test-plot-trajectory.R
@@ -1,5 +1,64 @@
 context("plot.trajectory")
 
+test_graph <- function(x, name, from, to) {
+  expect_true(inherits(plot(x), "htmlwidget"))
+
+  graph <- plot(x, output="DOT")
+  graph_lines <- strsplit(graph, "\n")[[1]]
+
+  nodes <- graph_lines[grep("label", graph_lines)]
+  id <- nodes %>%
+    sub("[[:space:]]*'", "", .) %>%
+    sub("' \\[.*", "", .) %>%
+    as.numeric()
+  label <- nodes %>%
+    sub(".*label = '", "", .) %>%
+    sub("'.*", "", .)
+  nodes <- data.frame(id=id, label=label) %>%
+    dplyr::arrange_("id")
+
+  edges <- graph_lines[grep("->", graph_lines)] %>%
+    sub(" \\[.*", "", .) %>%
+    gsub("'", "", .) %>%
+    strsplit("->") %>%
+    lapply(as.numeric) %>%
+    as.data.frame %>% t %>%
+    as.data.frame(stringsAsFactors = FALSE)
+  if (nrow(edges)) {
+    rownames(edges) <- NULL
+    colnames(edges) <- c("from", "to")
+    edges <- dplyr::arrange_(edges, "from", "to")
+  } else edges <- NULL
+
+  expect_true(all(nodes$label == name))
+  if (!is.null(edges)) {
+    expect_true(all(edges$from == from))
+    expect_true(all(edges$to == to))
+  } else {
+    expect_null(from)
+    expect_null(to)
+  }
+}
+
+test_that("a null trajectory fails", {
+  expect_error(plot(trajectory()))
+})
+
+test_that("a single-node trajectory is correctly converted to graph", {
+  x <- trajectory() %>%
+    timeout(1)
+
+  test_graph(x, c("Timeout"), NULL, NULL)
+})
+
+test_that("a simple trajectory is correctly converted to graph", {
+  x <- trajectory() %>%
+    seize("resource", 1) %>%
+    release("resource", 1)
+
+  test_graph(x, c("Seize", "Release"), 1, 2)
+})
+
 test_that("a complex trajectory is correctly converted to graph", {
   x <- trajectory() %>%
     seize("resource", 1) %>%
@@ -27,36 +86,10 @@ test_that("a complex trajectory is correctly converted to graph", {
     timeout(1) %>%
     rollback(2)
 
-  expect_true(inherits(plot(x), "htmlwidget"))
-
-  graph <- plot(x, output="DOT")
-  graph_lines <- strsplit(graph, "\n")[[1]]
-
-  nodes <- graph_lines[grep("label", graph_lines)]
-  id <- nodes %>%
-    sub("[[:space:]]*'", "", .) %>%
-    sub("' \\[.*", "", .) %>%
-    as.numeric()
-  label <- nodes %>%
-    sub(".*label = '", "", .) %>%
-    sub("'.*", "", .)
-  nodes <- data.frame(id=id, label=label) %>%
-    dplyr::arrange_("id")
-
-  edges <- graph_lines[grep("->", graph_lines)] %>%
-    sub(" \\[.*", "", .) %>%
-    gsub("'", "", .) %>%
-    strsplit("->") %>%
-    lapply(as.numeric) %>%
-    as.data.frame %>% t %>%
-    as.data.frame(stringsAsFactors = FALSE)
-  rownames(edges) <- NULL
-  colnames(edges) <- c("from", "to")
-  edges <- dplyr::arrange_(edges, "from", "to")
-
-  expect_true(all(nodes$label == c("Seize", "Timeout", "Release", "Branch", "Clone", "Seize", "Timeout",
-                                   "Release", "Trap", "Timeout", "Timeout", "SetAttribute", "SetAttribute",
-                                   "Seize", "Timeout", "Release", "Rollback", "Timeout", "Rollback")))
-  expect_true(all(edges$from == c(1, 2, 3, 4, 4, 4, 5, 5, 5, 6, 7, 8, 9, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19)))
-  expect_true(all(edges$to == c(2, 3, 4, 5, 12, 18, 6, 9, 18, 7, 8, 18, 10, 11, 18, 13, 14, 15, 16, 17, 1, 19, 4)))
+  test_graph(x,
+             c("Seize", "Timeout", "Release", "Branch", "Clone", "Seize", "Timeout",
+               "Release", "Trap", "Timeout", "Timeout", "SetAttribute", "SetAttribute",
+               "Seize", "Timeout", "Release", "Rollback", "Timeout", "Rollback"),
+             c(1, 2, 3, 4, 4, 4, 5, 5, 5, 6, 7, 8, 9, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19),
+             c(2, 3, 4, 5, 12, 18, 6, 9, 18, 7, 8, 18, 10, 11, 18, 13, 14, 15, 16, 17, 1, 19, 4))
 })
