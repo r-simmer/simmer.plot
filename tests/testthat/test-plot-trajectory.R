50ac249372aef6a9bd63b0da411f51a3f10967ac
Iñaki Úcar <i.ucar86@gmail.com>
2016-12-31 01:11:12 +0100

    add tests for rollbacks

--- tests/testthat/test-plot-trajectory.R
+++ tests/testthat/test-plot-trajectory.R
@@ -59,6 +59,14 @@ test_that("a simple trajectory is correctly converted to graph", {
   test_graph(x, c("Seize", "Release"), 1, 2)
 })
 
+test_that("a rollback with variable amount is correctly converted to graph", {
+  test_graph(trajectory() %>% rollback(0), c("Rollback"), 1, 1)
+  test_graph(trajectory() %>% rollback(1), c("Rollback"), 1, 1)
+  test_graph(trajectory() %>% timeout(1) %>% rollback(0), c("Timeout", "Rollback"), c(1, 2), c(2, 2))
+  test_graph(trajectory() %>% timeout(1) %>% rollback(1), c("Timeout", "Rollback"), c(1, 2), c(2, 1))
+  test_graph(trajectory() %>% timeout(1) %>% rollback(2), c("Timeout", "Rollback"), c(1, 2), c(2, 1))
+})
+
 test_that("a complex trajectory is correctly converted to graph", {
   x <- trajectory() %>%
     seize("resource", 1) %>%
