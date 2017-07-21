dbc204aa6e1b12dd2284860487b43f4dfcc38514
Iñaki Úcar <i.ucar86@gmail.com>
2017-07-21 12:47:15 +0200

    WIP: queuecomputer integration

---
+++ working_dir/queuecomputer_integration.R
@@ -0,0 +1,69 @@
+################################################################################
+# wrapper
+################################################################################
+
+QCWrap <- R6::R6Class("wrap",
+  public = list(
+    initialize = function(dp) private$dp <- dp,
+
+    get_mon_arrivals = function(...) {
+      private$dp$departures_df %>%
+        dplyr::rename(start_time = arrivals) %>%
+        dplyr::mutate(end_time = start_time + system_time,
+                      activity_time = system_time - waiting,
+                      finished = TRUE)
+    },
+
+    get_mon_resources = function(...) {
+      private$dp$queuelength_df %>%
+        dplyr::filter(times != dplyr::lead(times, default="1")) %>%
+        dplyr::rename(queue = queuelength) %>%
+        dplyr::right_join(private$dp$systemlength_df, by="times") %>% # this produces NAs, I don't understand why
+        tidyr::fill(queue) %>%                                        # I tried to fix them with fill(), but the result is not ok
+        dplyr::rename(time = times, system = queuelength) %>%
+        dplyr::mutate(resource = "QC",
+                      server = system - queue,
+                      capacity = private$dp$servers_input,
+                      queue_size = Inf,
+                      limit = Inf)
+    }
+  ),
+  private = list( dp = NULL )
+)
+
+qcwrap <- function(dp) QCWrap$new(dp)
+
+################################################################################
+# example
+################################################################################
+
+library(queuecomputer)
+library(simmer.plot)
+
+set.seed(42)
+arrivals <- cumsum(rexp(100))
+service <- rexp(100)
+departures <- queue_step(arrivals = arrivals, service = service)
+
+wrapped <- qcwrap(departures)
+plot(wrapped, what="arrivals")                                    # ok
+plot(wrapped, what="resources", metric="usage", "QC")             # not ok
+plot(wrapped, what="resources", metric="usage", "QC", steps=TRUE) # not ok
+
+################################################################################
+
+mm1 <- trajectory() %>%
+  seize("QC") %>%
+  timeout(function() rexp(1)) %>%
+  release("QC")
+
+set.seed(42)
+arrivals <- cumsum(rexp(100))
+env <- simmer() %>%
+  add_resource("QC") %>%
+  add_generator("arrival", mm1, at(arrivals)) %>%
+  run(Inf)
+
+plot(env, what="arrivals")
+plot(env, what="resources", metric="usage", "QC")
+plot(env, what="resources", metric="usage", "QC", steps=TRUE)
